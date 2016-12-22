58e2025fb3144d7366d0a14deb4f6a8a18819934
nikita <kaidash.nikita93@gmail.com>
2016-12-22 12:06:55 +0200

    create server and webpack config start

---
+++ src/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by nikita on 22.12.16.
+ */
+document.getElementById('root').innerHTML = 'Привет, я готов.';
+module.hot.accept();
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+/**
+ * Created by nikita on 22.12.16.
+ */
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        'babel-polyfill',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: { //Обновлено
+        loaders: [ //добавили babel-loader
+            {
+                loaders: ['babel-loader'],
+                include: [
+                    path.resolve(__dirname, "src"),
+                ],
+                test: /\.js$/,
+                plugins: ['transform-runtime'],
+            }
+        ]
+    }
+};
