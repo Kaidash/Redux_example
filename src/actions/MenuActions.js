16cb534504a06931894f84d03f79387723e2fa48
nikita <kaidash.nikita93@gmail.com>
2016-12-23 14:35:47 +0200

    add constants2

--- src/actions/MenuActions.js
+++ src/actions/MenuActions.js
@@ -1,10 +1,12 @@
 /**
  * Created by nikita on 22.12.16.
  */
+import { SET_MENU } from '../constants/Menu'
+
 export function setMenu(name) {
     console.log(name);
     return {
-        type: 'SET_MENU',
+        type: SET_MENU,
         payload: name
     }
 }
--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -1,10 +1,12 @@
 /**
  * Created by nikita on 23.12.16.
  */
+import { SET_USER } from '../constants/User'
+
 export function setUser(nameUser) {
     console.log(nameUser);
     return {
-        type: 'SET_USER',
+        type: SET_USER,
         payload: nameUser
     }
 }
